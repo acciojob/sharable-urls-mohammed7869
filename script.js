f3997fd35e9396241a9805de557f09bdff024fc0
mohammed7869 <122196547+mohammed7869@users.noreply.github.com>
2023-02-25 14:16:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // your code here
+var url = document.getElementById("url").textContent;
+
+function sharableURL() {
+	var nameElement = document.getElementById("name").value;
+	var yearElement = document.getElementById("year").value;
+	if(nameElement&&yearElement){
+		url="https://localhost:8080/?name="+nameElement+"&year="+yearElement;
+		document.getElementById("url").textContent = url;
+	}
+	else if(nameElement&&!yearElement){
+		url="https://localhost:8080/?name="+nameElement;
+		document.getElementById("url").textContent = url;
+	}
+	else if(yearElement&&!nameElement){
+		url="https://localhost:8080/?year="+yearElement;
+		document.getElementById("url").textContent = url;
+	}
+	else{
+		url="https://localhost:8080/";
+		document.getElementById("url").textContent = url;
+	}
+}
+
+document.getElementById("submit").addEventListener("click",sharableURL);
